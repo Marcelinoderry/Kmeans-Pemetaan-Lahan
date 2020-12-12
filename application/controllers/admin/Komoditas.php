79cb38202a51b83827b3b00accd5e5424b535c87
Alan Lengkoan <alanlengkoan15@gmail.com>
2020-12-13 01:22:17 +0800

    update

--- application/controllers/admin/Komoditas.php
+++ application/controllers/admin/Komoditas.php
@@ -36,27 +36,7 @@ class Komoditas extends MY_Controller
     // untuk get data serverside
     public function get_data_dt()
     {
-        $this->datatables->select('tb_kecamatan.nama AS kecamatan, tb_perkebunan.nama AS perkebunan, tb_komoditas.tahun, tb_komoditas.jumlah');
-        $this->datatables->join('tb_kecamatan', 'tb_komoditas.kd_kecamatan = tb_kecamatan.kd_kecamatan', 'left');
-        $this->datatables->join('tb_perkebunan', 'tb_komoditas.kd_perkebunan = tb_perkebunan.kd_perkebunan', 'left');
-
-        // untuk filter
-        if ($this->input->post('kecamatan')) {
-            $this->datatables->where('tb_kecamatan.kd_kecamatan', $this->input->post('kecamatan'));
-        }
-
-        if ($this->input->post('perkebunan')) {
-            $this->datatables->where('tb_perkebunan.kd_perkebunan', $this->input->post('perkebunan'));
-        }
-
-        if ($this->input->post('tahun')) {
-            $this->datatables->where('tb_komoditas.tahun', $this->input->post('tahun'));
-        }
-
-        $this->datatables->order_by('tb_kecamatan.nama');
-        $this->datatables->from('tb_komoditas');
-
-        return print_r($this->datatables->generate());
+        return $this->m_komoditas->getDataDt();
     }
 
     // untuk download format excel
--- application/models/M_komoditas.php
+++ application/models/M_komoditas.php
@@ -13,4 +13,29 @@ class M_komoditas extends CI_Model
         $result = $this->db->query("SELECT DISTINCT tahun FROM tb_komoditas")->result();
         return $result;
     }
+
+    public function getDataDt()
+    {
+        $this->datatables->select('tb_kecamatan.nama AS kecamatan, tb_perkebunan.nama AS perkebunan, tb_komoditas.tahun, tb_komoditas.jumlah');
+        $this->datatables->join('tb_kecamatan', 'tb_komoditas.kd_kecamatan = tb_kecamatan.kd_kecamatan', 'left');
+        $this->datatables->join('tb_perkebunan', 'tb_komoditas.kd_perkebunan = tb_perkebunan.kd_perkebunan', 'left');
+
+        // untuk filter
+        if ($this->input->post('kecamatan')) {
+            $this->datatables->where('tb_kecamatan.kd_kecamatan', $this->input->post('kecamatan'));
+        }
+
+        if ($this->input->post('perkebunan')) {
+            $this->datatables->where('tb_perkebunan.kd_perkebunan', $this->input->post('perkebunan'));
+        }
+
+        if ($this->input->post('tahun')) {
+            $this->datatables->where('tb_komoditas.tahun', $this->input->post('tahun'));
+        }
+
+        $this->datatables->order_by('tb_kecamatan.nama');
+        $this->datatables->from('tb_komoditas');
+
+        return print_r($this->datatables->generate());
+    }
 }
